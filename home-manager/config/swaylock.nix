5a45c278ff96d70c0c7643d9cb2dfa3543ef2ede
Theo Park <theo.park.n@gmail.com>
2026-03-18 15:28:02 -0400

    refactor(swaylock): rewrite set-wallpaper command in writeShellApplication

--- home-manager/config/swaylock.nix
+++ home-manager/config/swaylock.nix
@@ -1,41 +1,45 @@
-{ pkgs, config, ... }:
+{ config, lib, pkgs, ... }:
 
 let
-  set-wallpaper = pkgs.writeShellScriptBin "set-wallpaper.sh" ''
-    if [ -z "$1" ]; then
-      echo "Usage: set-wallpaper <image path>"
-      exit 1
-    fi
+  theo-set-wallpaper = pkgs.writeShellApplication {
+    name = "theo-set-wallpaper";
+    runtimeInputs = with pkgs; [ imagemagick swaybg dunst ];
+    text = ''
+      if [ -z "$1" ]; then
+        echo "Usage: theo-set-wallpaper <image path>"
+        exit 1
+      fi
 
-    ASSETS_DIR="''${XDG_DATA_HOME:-$HOME/.local/share}/theo-niri-assets"
-    mkdir -p "$ASSETS_DIR"
-    WALLPAPER="$ASSETS_DIR/wallpaper.png"
-    LOCKSCREEN="$ASSETS_DIR/lockscreen.png"
+      ASSETS_DIR="''${XDG_DATA_HOME:-$HOME/.local/share}/theo-niri-assets"
+      mkdir -p "$ASSETS_DIR"
+      WALLPAPER="$ASSETS_DIR/wallpaper.png"
+      LOCKSCREEN="$ASSETS_DIR/lockscreen.png"
 
-    # Back up existing wallpaper
-    if [ -f "$WALLPAPER" ]; then
-      TIMESTAMP=$(${pkgs.coreutils}/bin/date +%Y-%m-%d-%H-%M-%S)
-      cp "$WALLPAPER" "$ASSETS_DIR/wallpaper-$TIMESTAMP.png"
-    fi
+      # Back up existing wallpaper
+      if [ -f "$WALLPAPER" ]; then
+        TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+        cp "$WALLPAPER" "$ASSETS_DIR/wallpaper-$TIMESTAMP.png"
+      fi
 
-    # Attempt to change it to PNG
-    ${pkgs.imagemagick}/bin/magick "$1" "$WALLPAPER"
+      # Convert to change it to PNG
+      magick "$1" "$WALLPAPER"
 
-    # Generate lockscreen image with imagemagick and Nerdfont lock icon
-    ${pkgs.imagemagick}/bin/magick "$WALLPAPER"         \
-    -blur 0x8 -fill black -colorize 40%               \
-    -font "ProggyClean-Nerd-Font" -pointsize 180 -fill white -gravity center -annotate +0+0 "" \
-    "$LOCKSCREEN"
+      # Generate lockscreen image with imagemagick and Nerdfont lock icon
+      magick "$WALLPAPER"                       \
+      -blur 0x8 -fill black -colorize 40%     \
+      -font "ProggyClean-Nerd-Font" -pointsize 180 -fill white -gravity center -annotate +0+0 "" \
+      "$LOCKSCREEN"
 
-    # Respawn swaybg
-    pkill swaybg
-    ${pkgs.swaybg}/bin/swaybg --mode fill --image "$WALLPAPER" > /dev/null 2>&1 & disown
+      # Respawn swaybg (|| true for in case it failed to kill)
+      pkill swaybg || true
+      swaybg --mode fill --image "$WALLPAPER" > /dev/null 2>&1 & disown
 
-    ${pkgs.dunst}/bin/dunstify -a "System" -i "$WALLPAPER" "Wallpaper & lockscreen updated"
-  '';
+      dunstify -u low -i "$WALLPAPER" "Wallpaper & lockscreen updated"
+    '';
+  };
 in
 {
-  home.packages = [ set-wallpaper pkgs.swaybg ];
+  home.packages = [ theo-set-wallpaper pkgs.swaybg ];
 
   programs.swaylock = {
     enable = true;
@@ -47,7 +51,7 @@ in
       scaling = "fill";
       color = "39404F";
 
-      # automatically generated when changing wallpaper with set-wallpaper.sh
+      # automatically generated when changing wallpaper with theo-set-wallpaper
       image = "${config.home.homeDirectory}/.local/share/theo-niri-assets/lockscreen.png";
     };
   };
