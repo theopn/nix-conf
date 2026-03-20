46bfe45f306f95a978644675f2a1ff1ad3460b5a
Theo Park <theo.park.n@gmail.com>
2026-03-20 00:25:21 -0400

    feat(nixvim): add mini-clue config

--- home-manager/nixvim/plugins/mini.nix
+++ home-manager/nixvim/plugins/mini.nix
@@ -17,7 +17,6 @@
         };
 
         starter = {
-        # 
           # figlet -f small theovim
           header = ''
                      \/   \/
@@ -30,6 +29,54 @@
             |  _| ' \/ -_) _ \ V / | '  \
              \__|_||_\___\___/\_/|_|_|_|_|
             '';
+        };
+
+        clue = {
+          triggers = [
+            # leader
+            { mode = "n"; keys = "<Leader>"; }
+            { mode = "x"; keys = "<Leader>"; }
+            # square brackets
+            { mode = "n"; keys = "]"; }
+            { mode = "n"; keys = "["; }
+            # builtin completion
+            { mode = "i"; keys = "<C-x>"; }
+            # g
+            { mode = "n"; keys = "g"; }
+            { mode = "x"; keys = "g"; }
+            # marks
+            { mode = "n"; keys = "'"; }
+            { mode = "x"; keys = "'"; }
+            { mode = "n"; keys = "`"; }
+            { mode = "x"; keys = "`"; }
+            # registers
+            { mode = "n"; keys = "\""; }
+            { mode = "x"; keys = "\""; }
+            { mode = "i"; keys = "<C-r>"; }
+            # window commands
+            { mode = "n"; keys = "<C-w>"; }
+            # z
+            { mode = "n"; keys = "z"; }
+            { mode = "x"; keys = "z"; }
+          ];
+
+          clues = [
+            { __raw = "require('mini.clue').gen_clues.square_brackets()"; }
+            { __raw = "require('mini.clue').gen_clues.builtin_completion()"; }
+            { __raw = "require('mini.clue').gen_clues.g()"; }
+            { __raw = "require('mini.clue').gen_clues.marks()"; }
+            { __raw = "require('mini.clue').gen_clues.registers()"; }
+            { __raw = "require('mini.clue').gen_clues.windows()"; }
+            { __raw = "require('mini.clue').gen_clues.z()"; }
+          ];
+          
+          window = {
+            delay = 300;
+            config = {
+              width = "auto";
+              border = "rounded";
+            };
+          };
         };
 
       };
