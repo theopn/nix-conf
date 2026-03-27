194edd24543c4702f0c6b5b85164e0759e1aeca7
Theo Park <theo.park.n@gmail.com>
2026-03-27 13:14:04 -0400

    refactor(niri): remove dunstify volume and brightness control script. I really like those scripts, but they can be risky if the configuration is not deployed properly. I am typically very deliberate when I change volume or brightness, so lack of visual feedback shouldn't be too much of an issue.

--- home-manager/config/niri.nix
+++ home-manager/config/niri.nix
@@ -1,83 +1,6 @@
 { pkgs, ... }:
 
 let
-  theo-brightness-ctrl = pkgs.writeShellApplication{
-    name = "theo-brightness-ctrl";
-    runtimeInputs = with pkgs; [ brightnessctl dunst ];
-    text = ''
-      bar_color="#FFFFFC"
-
-      # brightnessctl get --percentage is only avilable in Git version,
-      # and they haven't done a release since 2024...
-      function get_brightness() {
-        max=$(brightnessctl max)
-        current=$(brightnessctl get)
-        echo $(( (current * 100 + max / 2) / max ))
-      }
-
-      function show_brightness_notif() {
-        brightness=$(get_brightness)
-        if [[ "$brightness" -le 25 ]]; then
-          icon="󰃞 "
-        elif [[ "$brightness" -le 50 ]]; then
-          icon="󰃟 "
-        elif [[ "$brightness" -le 75 ]]; then
-          icon="󰃝 "
-        else
-          icon="󰃠 "
-        fi
-
-        dunstify --timeout=1000 --replace=696969 -u low         \
-          "$icon Brightness: $brightness%"                      \
-          -h int:value:"''${brightness}" -h string:hlcolor:"''${bar_color}"
-
-      }
-
-      case $1 in
-        up)   brightnessctl set +5% ;;
-        down) brightnessctl set 5%- ;;
-        *)    echo "No argument specified" ;;
-      esac
-
-      show_brightness_notif
-  '';
-  };
-
-  theo-volume-ctrl = pkgs.writeShellApplication {
-    name = "theo-volume-ctrl";
-    runtimeInputs = with pkgs; [ brightnessctl dunst ];
-    text = ''
-      bar_color="#FFFFFF"
-      function get_volume() {
-        wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print int($2 * 100)}'
-      }
-      function get_mute() {
-        wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print ($3 == "[MUTED]" ? "yes" : "no")}'
-      }
-      function show_volume_notif() {
-        volume=$(get_volume)
-        mute=$(get_mute)
-        if [[ $volume -eq 0 ]] || [[ $mute == "yes" ]] ; then
-          volume_icon="󰖁  MUTED"
-        elif [[ $volume -lt 50 ]]; then
-          volume_icon="󰖀 "
-        else
-          volume_icon="󰕾 "
-        fi
-        dunstify --timeout=1000 --replace=6969 -u low         \
-          "$volume_icon Volume: ''${volume}%"                 \
-          -h int:value:"''${volume}" -h string:hlcolor:"''${bar_color}"
-      }
-      case $1 in
-        up)   wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+ ;;
-        down) wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%- ;;
-        mute) wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle ;;
-        *)    echo "No argument specified" ;;
-      esac
-      show_volume_notif
-    '';
-  };
-
   theo-rofi-powermenu = pkgs.writeShellApplication {
     name = "theo-rofi-powermenu";
     runtimeInputs = with pkgs; [ rofi ];
@@ -204,7 +127,7 @@ in
 {
 
   home.packages = with pkgs; [
-    theo-brightness-ctrl theo-volume-ctrl theo-rofi-powermenu theo-rofi-screenshot theo-rofi-screenrecord
+    theo-rofi-powermenu theo-rofi-screenshot theo-rofi-screenrecord
     brightnessctl pavucontrol playerctl
     grim slurp sway-contrib.grimshot wf-recorder wl-clipboard-rs
     nautilus networkmanagerapplet pantheon.pantheon-agent-polkit
